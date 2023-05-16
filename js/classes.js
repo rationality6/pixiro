61892564daee3043c910b84bc1d22dc131be3778
Hyun Ahn <codepawn@icloud.com>
2023-05-16 19:22:19 +0900

    working in progress

--- js/classes.js
+++ js/classes.js
@@ -1,16 +1,22 @@
 class Sprite {
-  constructor({ position, imageSrc, scale = 1, framesMax = 1, offset = { x: 0, y: 0 } }) {
-    this.position = position
-    this.width = 50
-    this.height = 150
-    this.image = new Image()
-    this.image.src = imageSrc
-    this.scale = scale
-    this.framesMax = framesMax
-    this.framesCurrent = 0
-    this.framesElapsed = 0
-    this.framesHold = 10
-    this.offset = offset
+  constructor({
+    position,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+  }) {
+    this.position = position;
+    this.width = 50;
+    this.height = 150;
+    this.image = new Image();
+    this.image.src = imageSrc;
+    this.scale = scale;
+    this.framesMax = framesMax;
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 10;
+    this.offset = offset;
   }
   draw() {
     ctx.drawImage(
@@ -22,117 +28,152 @@ class Sprite {
       this.position.x - this.offset.x,
       this.position.y - this.offset.y,
       (this.image.width / this.framesMax) * this.scale,
-      this.image.height * this.scale)
+      this.image.height * this.scale
+    );
   }
-  update() {
-    this.draw()
-    this.framesElapsed += 1
+
+  animateFrame() {
+    this.framesElapsed += 1;
     if (this.framesElapsed % this.framesHold === 0) {
       if (this.framesCurrent < this.framesMax - 1) {
-        this.framesCurrent += 1
+        this.framesCurrent += 1;
       } else {
-        this.framesCurrent = 0
+        this.framesCurrent = 0;
       }
     }
+  }
+
+  update() {
+    this.draw();
+    this.animateFrame();
   }
 }
 
 class Fighter extends Sprite {
-  constructor({ position, velocity, imageSrc, scale = 1, framesMax = 1, offset = { x: 0, y: 0 } }) {
+  constructor({
+    position,
+    velocity,
+    imageSrc,
+    scale = 1,
+    framesMax = 1,
+    offset = { x: 0, y: 0 },
+    sprites,
+  }) {
     super({
       position,
       imageSrc,
       scale,
       framesMax,
-      offset
-    })
-    this.hitpoint = 100
-    this.velocity = velocity
-    this.height = 150
-    this.width = 50
+      offset,
+    });
+    this.hitpoint = 100;
+    this.velocity = velocity;
+    this.height = 150;
+    this.width = 50;
     this.attackBox = {
       position: {
         x: this.position.x,
-        y: this.position.y
+        y: this.position.y,
       },
-      width: 180,
-      height: 60
-    }
-    this.isAttacking = false
+      width: 260,
+      height: 160,
+    };
+    this.isAttacking = false;
 
-    this.framesCurrent = 0
-    this.framesElapsed = 0
-    this.framesHold = 10
-  }
+    this.framesCurrent = 0;
+    this.framesElapsed = 0;
+    this.framesHold = 10;
 
-  animateFrame() {
-    this.framesElapsed += 1
-    if (this.framesElapsed % this.framesHold === 0) {
-      if (this.framesCurrent < this.framesMax - 1) {
-        this.framesCurrent += 1
-      } else {
-        this.framesCurrent = 0
-      }
+    this.sprites = sprites;
+
+    this.middle_of_attacking = false;
+    this.gatling_count = 0;
+    for (const sprite in sprites) {
+      this.sprites[sprite].image = new Image();
+      this.sprites[sprite].image.src = this.sprites[sprite].imageSrc;
     }
   }
 
   update() {
     // attack box
     if (this.isAttacking) {
-      ctx.strokeStyle = 'green'
+      ctx.strokeStyle = "green";
       ctx.lineWidth = 2;
-      ctx.strokeRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+      ctx.strokeRect(
+        this.attackBox.position.x,
+        this.attackBox.position.y,
+        this.attackBox.width,
+        this.attackBox.height
+      );
     }
 
     // body box
-    ctx.strokeStyle = 'red'
+    ctx.strokeStyle = "red";
     ctx.lineWidth = 2;
-    ctx.strokeRect(this.position.x, this.position.y, this.width, this.height)
+    ctx.strokeRect(this.position.x, this.position.y, this.width, this.height);
 
-    ctx.strokeStyle = 'blue'
+    ctx.strokeStyle = "blue";
     ctx.lineWidth = 2;
-    ctx.strokeRect(this.position.x + 1, this.position.y + 1, this.width + 1, this.height + 1)
+    ctx.strokeRect(
+      this.position.x + 1,
+      this.position.y + 1,
+      this.width + 1,
+      this.height + 1
+    );
 
     // hitpoint
-    ctx.fillStyle = 'white'
-    ctx.font = '20px Arial'
-    ctx.fillText(`HP: ${this.hitpoint}`, this.position.x, this.position.y - 10)
+    ctx.fillStyle = "white";
+    ctx.font = "20px Arial";
+    ctx.fillText(`HP: ${this.hitpoint}`, this.position.x, this.position.y - 10);
 
-    this.position.x += this.velocity.x
-    this.position.y += this.velocity.y
-    this.attackBox.position.x = this.position.x - 30
-    this.attackBox.position.y = this.position.y + 30
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.attackBox.position.x = this.position.x;
+    this.attackBox.position.y = this.position.y;
 
-    this.floor_collision_detection_and_gravity()
-    this.wall_collision_detection()
-    this.draw()
-    this.animateFrame()
+    this.floor_collision_detection_and_gravity();
+    this.wall_collision_detection();
+    this.draw();
+    this.animateFrame();
   }
 
   floor_collision_detection_and_gravity() {
-    const ground = 100
-    if (this.position.y + this.height + this.velocity.y >= canvas.height - ground) {
-      this.velocity.y = 0
+    const ground = 100;
+    if (
+      this.position.y + this.height + this.velocity.y >=
+      canvas.height - ground
+    ) {
+      this.velocity.y = 0;
     } else {
-      this.velocity.y += gravity
+      this.velocity.y += gravity;
     }
   }
 
   attack() {
-    this.isAttacking = true
-    setTimeout(() => this.isAttacking = false, 200)
+    this.isAttacking = true;
+    setTimeout(() => (this.isAttacking = false), 200);
+  }
+
+  gatling_attack() {
+    this.middle_of_attacking = true;
+    const timeoutHandler = setTimeout(() => (this.middle_of_attacking = false), 1000);
+    if (this.middle_of_attacking) {
+      console.log("middle of attacking");
+    } else {
+      window.clearTimeout(timeoutHandler);
+      const timeoutHandler = setTimeout(() => (this.middle_of_attacking = false), 1000);
+    }
   }
 
   wall_collision_detection() {
     // right side
     if (this.position.x + this.width >= canvas.width) {
-      this.position.x = canvas.width - this.width
+      this.position.x = canvas.width - this.width;
     }
 
     // left side
     if (this.position.x <= 0) {
-      this.position.x = 0
+      this.position.x = 0;
     }
   }
-
 }
--- js/logic.js
+++ js/logic.js
@@ -1,123 +1,140 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-const gravity = 0.8
+const gravity = 0.8;
 
-const background = new Sprite(
-  {
-    position: { x: 0, y: 0 },
-    imageSrc: './assets/background.png'
-  }
-)
+const background = new Sprite({
+  position: { x: 0, y: 0 },
+  imageSrc: "./assets/background.png",
+});
 
-const shop = new Sprite(
-  {
-    position: { x: 600, y: 128 },
-    imageSrc: './assets/shop.png',
-    scale: 2.75,
-    framesMax: 6
-  }
-)
+const shop = new Sprite({
+  position: { x: 600, y: 128 },
+  imageSrc: "./assets/shop.png",
+  scale: 2.75,
+  framesMax: 6,
+});
 
-const player = new Fighter(
-  {
-    position: { x: 100, y: 100 },
-    velocity: { x: 0, y: 0 },
-    imageSrc: './assets/samuraiMack/Idle.png',
-    framesMax: 8,
-    scale: 2.5,
-    offset: { x: 215, y: 152 }
+const player = new Fighter({
+  position: { x: 100, y: 100 },
+  velocity: { x: 0, y: 0 },
+  imageSrc: "./assets/samuraiMack/Idle.png",
+  framesMax: 8,
+  scale: 2.5,
+  offset: { x: 215, y: 152 },
+  sprites: {
+    idle: {
+      imageSrc: "./assets/samuraiMack/Idle.png",
+      framesMax: 8,
+    },
+    run: {
+      imageSrc: "./assets/samuraiMack/Run.png",
+      framesMax: 8,
+    },
+    attack: {
+      imageSrc: "./assets/samuraiMack/Attack1.png",
+      framesMax: 6,
+    },
+    attack2: {
+      imageSrc: "./assets/samuraiMack/Attack2.png",
+      framesMax: 6,
+    }
   }
-)
+});
 
-const enermy = new Fighter(
-  {
-    position: { x: 800, y: 100 },
-    velocity: { x: 0, y: 0 },
-    imageSrc: './assets/kenji/Idle.png',
-    framesMax: 4,
-    scale: 2.5,
-    offset: { x: 215, y: 152 }
-  }
-)
+const enermy = new Fighter({
+  position: { x: 800, y: 100 },
+  velocity: { x: 0, y: 0 },
+  imageSrc: "./assets/kenji/Idle.png",
+  framesMax: 4,
+  scale: 2.5,
+  offset: { x: 215, y: 152 },
+});
 
 const reactangularCollisionDetection = ({ rect1, rect2 }) => {
-  return (rect1.attackBox.position.x + rect1.attackBox.width >= rect2.position.x
-    && rect1.attackBox.position.x <= rect2.position.x + rect2.width
-    && rect1.attackBox.position.y + rect1.attackBox.height >= rect2.position.y
-    && rect1.attackBox.position.y <= rect2.position.y + rect2.height)
-}
+  return (
+    rect1.attackBox.position.x + rect1.attackBox.width >= rect2.position.x &&
+    rect1.attackBox.position.x <= rect2.position.x + rect2.width &&
+    rect1.attackBox.position.y + rect1.attackBox.height >= rect2.position.y &&
+    rect1.attackBox.position.y <= rect2.position.y + rect2.height
+  );
+};
 
 const hit_detection = () => {
-  if (reactangularCollisionDetection({ rect1: player, rect2: enermy }) && player.isAttacking) {
-    new Audio('./assets/sounds/dash.wav').play()
-    enermy.hitpoint -= 15
-    player.isAttacking = false
+  if (
+    reactangularCollisionDetection({ rect1: player, rect2: enermy }) &&
+    player.isAttacking
+  ) {
+    // new Audio("./assets/sounds/dash.wav").play();
+    new Audio("./assets/sounds/hit.wav").play();
+    enermy.hitpoint -= 15;
+    player.isAttacking = false;
   } else {
-    player.isAttacking = false
+    player.isAttacking = false;
   }
-}
-
+};
 
 const object_collision_detection = ({ player, enermy }) => {
   if (player.position.x + player.width > enermy.position.x) {
-    player.position.x -= player.position.x + player.width
+    player.position.x -= player.position.x + player.width;
   }
 
   // // left side
   // if (player.position.x <= 0) {
   //   player.position.x = 0
   // }
-
-}
+};
 
 const animate = () => {
-  window.requestAnimationFrame(animate)
+  window.requestAnimationFrame(animate);
 
-  background.update()
-  shop.update()
+  background.update();
+  shop.update();
 
-  player.update()
-  enermy.update()
+  player.update();
+  enermy.update();
 
-  object_collision_detection({ player, enermy })
+  object_collision_detection({ player, enermy });
 
-  hit_detection()
-}
+  hit_detection();
+};
 
-animate()
+animate();
 
-window.addEventListener('keydown', (e) => {
-  if (e.key === 'ArrowUp') {
+window.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowUp") {
     if (player.velocity.y === 0) {
-      player.velocity.y = -15
+      player.velocity.y = -15;
     }
   }
 
-  if (e.key === 'ArrowRight') {
-    player.velocity.x = 7
+  if (e.key === "ArrowRight") {
+    player.velocity.x = 7;
   }
 
-  if (e.key === 'ArrowLeft') {
-    player.velocity.x = -7
+  if (e.key === "ArrowLeft") {
+    player.velocity.x = -7;
   }
 
   if (e.code == "Space") {
-    player.attack()
+    player.image = player.sprites.attack.image;
+    player.framesMax = player.sprites.attack.framesMax;
+    player.framesHold = 3;
+    
+    player.attack();
   }
+});
 
-})
-
-window.addEventListener('keyup', (e) => {
-  if (e.key === 'ArrowRight') {
-    player.velocity.x = 0
+window.addEventListener("keyup", (e) => {
+  if (e.key === "ArrowRight") {
+    player.velocity.x = 0;
   }
-  if (e.key === 'ArrowLeft') {
-    player.velocity.x = 0
+  if (e.key === "ArrowLeft") {
+    player.velocity.x = 0;
   }
-})
+});
